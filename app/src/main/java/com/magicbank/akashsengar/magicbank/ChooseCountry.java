fc7b90c62b53d9d4215038959cb900baff9d85dc
akash14204 <akashsengar786@gmail.com>
2017-07-24 11:31:53 +0530

    Initial commit

---
+++ app/src/main/java/com/magicbank/akashsengar/magicbank/ChooseCountry.java
@@ -0,0 +1,141 @@
+package com.magicbank.akashsengar.magicbank;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ChooseCountry extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    private Spinner spinner;
+    private static final String[]paths = {"Choose Country","India", "Philippines", "UK","Sweden", "Canada", "Australia", "Serbia", "Malaysia","Maldives"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_country);
+
+        final CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+        sharedPreferences=getSharedPreferences("DATABASE",MODE_PRIVATE);
+        final SharedPreferences.Editor se = sharedPreferences.edit();
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(ChooseCountry.this, android.R.layout.simple_spinner_item,paths);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, final int i, long l) {
+                switch (i) {
+                    case 0:
+                        // Whatever you want to happen when the first item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 1:
+                        // Whatever you want to happen when the first item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 2:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 3:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 4:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 5:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 6:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 7:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 8:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                    case 9:
+                        // Whatever you want to happen when the second item gets selected
+                        se.putInt("flag_value",i);
+                        se.commit();
+                        break;
+                }
+
+                Button btn = (Button) findViewById(R.id.main_custom_button);
+                assert btn != null;
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(i!=0 && checkBox.isChecked()) {
+                            Intent intent9 = new Intent(ChooseCountry.this, MainActivity.class);
+                            startActivity(intent9);
+                            finish();
+                            overridePendingTransition(R.anim.enter, R.anim.exit);
+                            Toast.makeText(ChooseCountry.this, "All set ,welcome to MagicBank", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(ChooseCountry.this, "Choose country and Check Checkbox", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
+
+    }
+
+    //for two button exit
+    private long lastPressedTime;
+    private static final int PERIOD = 2000;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            switch (event.getAction()) {
+                case KeyEvent.ACTION_DOWN:
+                    if (event.getDownTime() - lastPressedTime < PERIOD) {
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Press again to exit.", Toast.LENGTH_SHORT).show();
+                        lastPressedTime = event.getEventTime();
+                    }
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+}
